dc7be2534c6496f7fe70ed4c5204a2fc2fd6b508
lavrentiev-d <denlavre@mail.ru>
2023-04-23 15:06:02 +0300

    forecasts_by_city.dart добавлен фильтр прогнозов (прогноз на 3 дня)

--- packages/search_forecast_repository/lib/src/models/forecasts_by_city.dart
+++ packages/search_forecast_repository/lib/src/models/forecasts_by_city.dart
@@ -18,8 +18,19 @@ class ForecastsByCity {
           message: json['message'].toString(),
           city: City.fromJson(json['city']),
           forecasts: json['list']
-              .map<Forecast>((forecast) => Forecast.fromMap(forecast))
+              .map<Forecast>((forecast) => Forecast.fromJson(forecast))
+              .where(_isForecastBy3Days)
               .toList());
+
+  static bool _isForecastBy3Days(Forecast forecast) {
+    final now = DateTime.now();
+    final forecastDateTime = DateTime.parse(forecast.dtTxt);
+    var lastDate = DateTime(now.year, now.month, now.day, 0, 0, 0);
+    lastDate = lastDate.add(Duration(days: 2));
+    final forecastDate = DateTime(
+        forecastDateTime.year, forecastDateTime.month, forecastDateTime.day, 0);
+    return !lastDate.difference(forecastDate).inDays.isNegative;
+  }
 
   Map<String, dynamic> toJson() =>
       {'cod': code, 'message': message, 'city': city, 'list': forecasts};
